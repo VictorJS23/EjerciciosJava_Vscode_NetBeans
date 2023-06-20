54b72149d49484be34533c8e4ac5a0c003f99410
VictorJS <vb329708@gmail.com>
2023-06-19 21:52:19 -0600

    examen

---
+++ normalesYalgunasExplicaciones/ejer13ExamenDos.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class ejer13ExamenDos {
+    public static void main(String[] args) {
+        /*
+         * - Encontrar la secuencia creciente ininterrumpida más larga: Implementa un
+         * método que encuentre la secuencia
+         * creciente más larga en un arreglo de números enteros. La secuencia creciente
+         * es una secuencia de
+         * números en la que cada número es mayor que el anterior. Por ejemplo, en el
+         * arreglo
+         * [1, 3, 2, 4, 8, 7, 9], la secuencia creciente más larga sería 2, 4, 8.
+         * Si tuviéramos [1, 3, 2, 4, 8, 7, 9, 11, 25, 200], la secuencia más larga
+         * seria 7, 9, 11, 25, 200
+         * 
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        int[] arreglo = new int[6];
+
+        System.out.println("Ingrese los valores del arreglo");
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = input.nextInt();
+        }
+
+        int cont = 0;
+
+        for (int i = 0; i < arreglo.length - 1; i++) {
+            if (arreglo[i] < arreglo[i + 1]) {
+                cont++;
+            }
+        }
+
+        int[] arregloDos = new int[cont];
+
+        for (int i = 0; i < arreglo.length - 1; i++) {
+            if (arreglo[i] < arreglo[i + 1]) {
+                arregloDos[i] = arreglo[i];
+            }
+        }
+
+        System.out.println("la secuencia más larga seria");
+        System.out.println("");
+
+        for (int i = 0; i < arregloDos.length; i++) {
+            System.out.println(arregloDos[i]);
+        }
+
+        input.close();
+
+    }
+} // Aun no imprime bien los valores
