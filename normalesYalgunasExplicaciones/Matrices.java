83c2fabd288009bcbdae8ce4e20ec3fcfc4edcfe
VictorJS <vb329708@gmail.com>
2023-06-21 16:03:35 -0600

    edit matriz

--- normalesYalgunasExplicaciones/Matrices.java
+++ normalesYalgunasExplicaciones/Matrices.java
@@ -1,4 +1,3 @@
-package matrices;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -9,43 +8,44 @@ public class Matrices {
         int matriz[][];
         int nFilas;
         int nColumnas;
-        
-        
+
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("Ingrese el numero de filas");
         nFilas = input.nextInt();
         System.out.println("");
-        
+
         System.out.println("Ingrese el numero de columnas");
         nColumnas = input.nextInt();
         System.out.println("");
-        
+
         matriz = new int[nFilas][nColumnas];
-        
+
         System.out.println("Ingrese los elementos de la matriz : ");
-        for(int i = 0; i < nFilas; i++){
-            for(int j = 0; j < nColumnas; j++){
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nColumnas; j++) {
                 System.out.print("posicion [" + i + "] [" + j + "] : ");
                 matriz[i][j] = input.nextInt();
             }
         }
-        
+
         System.out.println("\n La matriz resultante es: ");
-        
-        for(int i = 0; i < nFilas; i++){
-            for(int j = 0; j < nColumnas; j++){
+
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nColumnas; j++) {
                 System.out.print(matriz[i][j] + " , ");
             }
             System.out.println("");
         }
-        
+
         System.out.println("");
-        
+
         System.out.println("Usando el metodo llamado Arrays.deepToString : ");
-        
+
         System.out.println(Arrays.deepToString(matriz));
-        
+
+        input.close();
+
     }
-    
-} 
+
+}
--- normalesYalgunasExplicaciones/mostrarTraspuesta.java
+++ normalesYalgunasExplicaciones/mostrarTraspuesta.java
@@ -1,78 +1,80 @@
-package matrices;
 
 import java.util.Scanner;
 
 public class mostrarTraspuesta {
 
     public static void main(String[] args) {
-        
-        int array [][] = new int[3][3];
-        
+
+        int array[][] = new int[3][3];
+
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("Ingresa un numero en la posicion: ");
         System.out.println("");
-        
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                System.out.print("posicion [" + i + "] [" + j + "] : " );
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("posicion [" + i + "] [" + j + "] : ");
                 array[i][j] = input.nextInt();
             }
         }
-        
+
         System.out.println("");
         System.out.println("La matriz original es: ");
-        
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(array[i][j] + "  ");
             }
             System.out.println("");
         }
-        
+
         System.out.println("");
-        
+
         int aux;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < i; j++){
-            //Se pone j<i ya que en la segunda iteracion se pondria igual a la matriz original
-            //por lo tanto se saca de la diagonal principal hacia abajo
-            //Esta condición asegura que el bucle interno solo itere para las columnas que están por debajo de la diagonal principal.
-                
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < i; j++) {
+                // Se pone j<i ya que en la segunda iteracion se pondria igual a la matriz
+                // original
+                // por lo tanto se saca de la diagonal principal hacia abajo
+                // Esta condición asegura que el bucle interno solo itere para las columnas que
+                // están por debajo de la diagonal principal.
+
                 aux = array[i][j];
                 array[i][j] = array[j][i];
                 array[j][i] = aux;
             }
         }
-        
+
         System.out.println("La matriz traspuesta es : ");
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(array[i][j] + "  ");
             }
             System.out.println("");
         }
-        
+
         System.out.println("");
-        
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3;j++){
-                if(i == j){
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
                     array[i][j] = 1;
                 } else {
                     array[i][j] = 0;
                 }
             }
         }
-        
+
         System.out.println("La matriz identidad  es : ");
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(array[i][j] + "  ");
             }
             System.out.println("");
         }
-        
+
+        input.close();
     }
-    
+
 }
