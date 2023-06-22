065008383fb8842f9f550ae78938a973192c2731
VictorJS <vb329708@gmail.com>
2023-06-21 23:59:13 -0600

    .

--- normalesYalgunasExplicaciones/ejer13ExamenDos.java
+++ normalesYalgunasExplicaciones/ejer13ExamenDos.java
@@ -10,6 +10,7 @@ public class ejer13ExamenDos {
          * números en la que cada número es mayor que el anterior. Por ejemplo, en el
          * arreglo
          * [1, 3, 2, 4, 8, 7, 9], la secuencia creciente más larga sería 2, 4, 8.
+         * 
          * Si tuviéramos [1, 3, 2, 4, 8, 7, 9, 11, 25, 200], la secuencia más larga
          * seria 7, 9, 11, 25, 200
          * 
@@ -17,7 +18,7 @@ public class ejer13ExamenDos {
 
         Scanner input = new Scanner(System.in);
 
-        int[] arreglo = new int[6];
+        int[] arreglo = new int[7];
 
         System.out.println("Ingrese los valores del arreglo");
         for (int i = 0; i < arreglo.length; i++) {
@@ -33,21 +34,30 @@ public class ejer13ExamenDos {
         }
 
         int[] arregloDos = new int[cont];
+        int j = 0;
 
         for (int i = 0; i < arreglo.length - 1; i++) {
             if (arreglo[i] < arreglo[i + 1]) {
-                arregloDos[i] = arreglo[i];
+                arregloDos[j] = arreglo[i];
+                j++;
             }
         }
 
-        System.out.println("la secuencia más larga seria");
-        System.out.println("");
+        if (arreglo.length > 0 && arreglo[arreglo.length - 1] > arregloDos[j - 1]) {
+            arregloDos[j] = arreglo[arreglo.length - 1];
+            j++;
+        }
+
+        System.out.println("La secuencia más larga sería:");
+        System.out.println();
 
         for (int i = 0; i < arregloDos.length; i++) {
             System.out.println(arregloDos[i]);
         }
 
         input.close();
-
     }
-} // Aun no imprime bien los valores
+}
+// Aun no imprime bien los valores
+
+// aun no imprime nada
---
+++ normalesYalgunasExplicaciones/xd.java
@@ -0,0 +1,55 @@
+public class xd {
+    public static void main(String[] args) {
+        int[] arreglo1 = { 1, 3, 2, 4, 8, 7, 9 };
+        int[] arreglo2 = { 1, 3, 2, 4, 8, 7, 9, 11, 25, 200 };
+
+        int[] secuencia1 = encontrarSecuenciaCreciente(arreglo1);
+        int[] secuencia2 = encontrarSecuenciaCreciente(arreglo2);
+
+        System.out.println("Secuencia creciente más larga en arreglo1:");
+        imprimirSecuencia(secuencia1);
+
+        System.out.println("\nSecuencia creciente más larga en arreglo2:");
+        imprimirSecuencia(secuencia2);
+    }
+
+    public static int[] encontrarSecuenciaCreciente(int[] arreglo) {
+        int longitudMaxima = 0;
+        int indiceInicio = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            int longitudActual = 1;
+            int indiceActual = i;
+
+            for (int j = i; j < arreglo.length - 1; j++) {
+                if (arreglo[j] < arreglo[j + 1]) {
+                    longitudActual++;
+                } else {
+                    break;
+                }
+            }
+
+            if (longitudActual > longitudMaxima) {
+                longitudMaxima = longitudActual;
+                indiceInicio = indiceActual;
+            }
+        }
+
+        int[] secuencia = new int[longitudMaxima];
+        for (int i = 0; i < longitudMaxima; i++) {
+            secuencia[i] = arreglo[indiceInicio + i];
+        }
+
+        return secuencia;
+    }
+
+    public static void imprimirSecuencia(int[] secuencia) {
+        for (int i = 0; i < secuencia.length; i++) {
+            System.out.print(secuencia[i]);
+
+            if (i < secuencia.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
