0fe25f30aab113907c704f7b607638bad21ecd63
VictorJS <vb329708@gmail.com>
2023-07-06 23:21:53 -0600

    commit

---
+++ normalesYalgunasExplicaciones/anagrama.java
@@ -0,0 +1,18 @@
+import arrays.Scanenr;
+
+public class anagrama {
+    public static void main(String[] args) {
+
+        /*
+         * Dadas dos cadenas de caracteres, implementa un programa en Java que determine
+         * si son anagramas, es decir, si contienen exactamente las mismas letras pero
+         * en un orden diferente. El programa debe mostrar "true" si son anagramas, y
+         * "false" en caso contrario.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printl("Ingrese una cadena: ");
+
+    }
+}
